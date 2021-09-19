c48ed9cb8dec63b8c2c36b70e3682ea65081f4e7
nangongtianyi <workmailofyi@163.com>
2021-09-19 22:13:02 +0800

    file: Update the files

--- Head First C++/chap6/p1/command.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMMAND_H__
-#define __COMMAND_H__
-
-class Command {
-public:
-    virtual void execute() = 0;
-};
-
-#endif
--- Head First C++/chap7/p2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "homeTheaterFacade.h"
-
-int main() {
-    DvdPlayer *dvd = new DvdPlayer();
-    Screen *screen = new Screen();
-    HomeTheaterFacade *homeTheaterFacade = new HomeTheaterFacade(dvd, screen);
-    homeTheaterFacade->watchMovie();
-    homeTheaterFacade->endMovie();
-}

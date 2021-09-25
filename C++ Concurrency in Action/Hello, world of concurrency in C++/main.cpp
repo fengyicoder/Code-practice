1dd176735e53ae92b64812a14c426c3ff36a9ed1
nangongtianyi <workmailofyi@163.com>
2021-09-25 12:10:50 +0800

    feature: build fold of new book

---
+++ C++ Concurrency in Action/Hello, world of concurrency in C++/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+
+void hello() {
+    std::cout << "Hello Concurrent World\n";
+}
+
+int main() 
+{
+    std::thread t(hello);
+    t.join();
+
+    return 0;
+}

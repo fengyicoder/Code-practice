ac97f26f79f2b1b9c2340850e700c5bea5e5c1b4
nangongtianyi <workmailofyi@163.com>
2021-12-11 21:42:00 +0800

    feature: Synchronizing concurrent operations

--- C++ Concurrency in Action/Synchronizing concurrent operations/exp5.cpp
+++ C++ Concurrency in Action/Synchronizing concurrent operations/exp5.cpp
@@ -60,7 +60,7 @@ std::list<T> parallel_quick_sort(std::list<T> input)
     std::list<T> lower_part;
     lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
     std::future<std::list<T>> new_lower(
-        std::async(&parallel_quick<T>, std::move(lower_part)));
+        std::async(&parallel_quick_sort<T>, std::move(lower_part)));
     auto new_higher(parallel_quick_sort(std::move(input)));
     result.splice(result.end(), new_higher);
     result.splice(result.begin(), new_lower.get());

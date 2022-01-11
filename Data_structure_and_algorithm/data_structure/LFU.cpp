1e95ea5b8e7f0bb594f2af1374ee7311a6a1f666
nangongtianyi <workmailofyi@163.com>
2022-01-11 08:49:12 +0800

    feature: Add LFU part3

--- Data_structure_and_algorithm/data_structure/LFU.cpp
+++ Data_structure_and_algorithm/data_structure/LFU.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+#ifdef KV
 template <class KeyType, class MappedType,
             class Hash = std::hash<KeyType>,
             class Pred = std::equal_to<KeyType> >
@@ -99,7 +100,126 @@ public:
         _map_key2entry.clear();
         _size = 0;
     }
-}
+
+    inline size_t size() {
+        return _size;
+    }
+
+    inline bool empty() const {
+        return _values.empty();
+    }
+
+};
+
+#else
+
+template <class KeyType,
+            class Hash = std::hash<KeyType>,
+            class Pred = std::equal_to<KeyType> >
+class LinkedHashSet
+{
+public:
+    typedef KeyType EntryType;
+    typedef typename std::list<EntryType>::iterator iterator;
+    typedef typename std::list<EntryType>::const_iterator const_iterator;
+
+private:
+    typedef std::unordered_map<KeyType, iterator, Hash, Pred> HashMap;
+    typedef typename HashMap::iterator _map_itr;
+    typedef typename HashMap::const_iterator _map_citr;
+    size_t _size;
+    std::list<EntryType> _values;
+    HashMap _map_key2entry;
+
+public:
+    std::pair<iterator, bool> insert(const EntryType& new_entry) {
+        _map_itr pIt = _map_key2entry.find(new_entry);
+        if (pIt == _map_key2entry.end()) {
+            _values.push_back(new_entry);
+            _map_key2entry.insert(std::pair<KeyType, iterator>(new_entry, --_ _values.end()));
+            ++_size;
+        }
+        else {
+            _values.erase(pIt->second);
+            _values.push_back(new_entry);
+            pIt->second = --_values.end();
+        }
+        return std::pair<iterator, bool>(--_values.end(), true);
+    }
+
+    iterator insert(const_iterator it, const EntryType& new_entry) {
+        _map_itr pIt = _map_key2entry.find(new_entry);
+        if(pIt != _map_key2entry.end()) {
+            _values.erase(pIt->second);
+            --_size;
+        }
+        iterator _it = _values.insert(it, new_entry);
+        _map_key2entry.insert(std::pair<KeyType, iterator>(new_entry, _it));
+        ++_size;
+        return _it;
+    }
+
+    inline iterator begin() { return _values.begin();}
+    inline const_iterator begin() const { return _values.begin();}
+    inline iterator end() { return _values.end();}
+    inline const_iterator end() const { return _values.end();}
+    
+    iterator find(const KeyType& key) {
+        if (_size > 0) {
+            _map_itr it = _map_key2entry.find(key);
+            if (it == _map_key2entry.end()) return _map_key2entry.end();
+            iterator find_it = it.second;
+            return find_it;
+        }
+        return _values.end();
+    }
+
+    const_iterator find(const KeyType& key) const {
+        if (_size > 0) {
+            _map_citr it = _map_key2entry.find(key);
+            if (it == _map_key2entry.end()) {
+                return _values.end();
+            }
+            const_iterator find_it = it->second;
+            return find_it;
+        }
+        return _values.end();
+    }
+
+    void erase(iterator pos) {
+        assert(pos != _values.end());
+        assert(_size != 0);
+
+        EntryType v = *pos;
+        _values.erase(pos);
+        _map_key2entry.erase(v);
+        --_size;
+    }   
+
+    void erase(const KeyType& key) {
+        iterator it = find(key);
+        if (it != _values.end()) {
+            erase(it);
+        }
+
+    }
+
+    void clear() {
+        _values.clear();
+        _map_key2entry.clear();
+        _size = 0;
+    }
+
+    inline size_t size() {
+        return _size;
+    }
+
+    inline bool empty() const {
+        return _values.empty();
+    }
+};
+
+#endif
 
 struct LFUCache
 {
@@ -108,7 +228,7 @@ struct LFUCache
     // key 到 freq 的映射，我们后文称为 KF 表
     unordered_map<int, int> keyToFreq;
     // freq 到 key 列表的映射，我们后文称为 FK 表
-    unordered_map<int, LinkedHashSet<int, int> > freqToKeys;
+    unordered_map<int, LinkedHashSet<int> > freqToKeys;
     // 记录最小的频次
     int minFreq;
     // 记录 LFU 缓存的最大容量
@@ -126,5 +246,43 @@ struct LFUCache
         return keyToVal[key];
     }
 
-    void put(int key, int val) {}
+    void put(int key, int val) {
+        if (cap <= 0) return;
+        if (keyToVal.count(key)) {
+            keyToVal[key] = val;
+            increaseFreq(key);
+        }
+
+        if (cap <= keyToVal.size()) {
+            removeMinFreqKey();
+        }
+        keyToVal.insert(std::make_pair(key, val));
+        keyToFreq.insert(std::make_pair(key, 1));
+        freqToKeys.insert(std::make_pair(1, LinkedHashSet<int>()));
+        freqToKeys[1].insert(key);
+        minFreq = 1;
+    }
+
+    void increaseFreq(int key) {
+        int freq = keyToFreq[key];
+        keyToFreq[key] = freq + 1;
+        freqToKeys[freq].erase(key);
+        freqToKeys.insert(std::make_pair(freq+1, LinkedHashSet<int>()));
+        freqToKeys[freq+1].insert(key);
+        if (freqToKeys[freq].empty()) {
+            freqToKeys.erase(freq);
+            if (freq == minFreq) minFreq++;
+        }
+    }
+
+    void removeMinFreqKey() {
+        LinkedHashSet<int> keyList = freqToKeys[minFreq];
+        int deleteKey = *keyList.begin();
+        keyList.erase(deleteKey);
+        if (keyList.empty()) {
+            freqToKeys.erase(minFreq);
+        }
+        keyToFreq.erase(deleteKey);
+        keyToVal.erase(deleteKey);
+    }
 };

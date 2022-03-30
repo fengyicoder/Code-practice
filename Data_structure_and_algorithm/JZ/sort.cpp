0daf89e27308cf0e3fcbed794d3eb7fcb0a08ab0
nangongtianyi <workmailofyi@163.com>
2022-03-30 08:21:58 +0800

    feature: add new algrithom

--- Data_structure_and_algorithm/JZ/sort.cpp
+++ Data_structure_and_algorithm/JZ/sort.cpp
@@ -92,6 +92,52 @@ void quick_sort(int q[], int l, int r) {
     quick_sort(q, j+1, r);
 }
 
+//非递归
+int partion(int* root,int low,int high)
+{
+    int part=root[low];
+    while(low<high)
+    {
+        while(low<high&&root[high]>part) high--;
+        root[low]=root[high];
+        while(low<high&&root[low]<part) low++;
+        root[high]=root[low];
+    }
+    root[low]=part;
+    return low;
+}
+ 
+void quickSort2(int* root,int low,int high)
+{
+    stack<int> st;
+    int k;
+    if(low<high)
+    {
+        st.push(low);
+        st.push(high);
+        while(!st.empty())
+        {
+            int j=st.top();st.pop();
+            int i=st.top();st.pop();
+            
+            k=partion(root,i,j);
+            
+            if(i<k-1)
+            {
+                st.push(i);
+                st.push(k-1);
+            }
+            if(k+1<j)
+            {
+                st.push(k+1);
+                st.push(j);
+            }
+        }
+    
+    }
+  
+}
+
 //2. 归并排序 稳定 O(nlgn)
 void merge_sort(int q[], int l, int r) {
     if (l >= j) return;
